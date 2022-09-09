7927c791d6ad32468a62357615fb31ac66f320ba
TSuhas <suhastupake07@gmail.com>
2022-09-09 17:15:52 +0530

    seventh commit

---
+++ src/main/java/MavenProject/ExtendReports/App.java
@@ -0,0 +1,13 @@
+package MavenProject.ExtendReports;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
